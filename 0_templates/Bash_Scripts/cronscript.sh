bc1540d4ae7f862edc6faa544049ba39e331a62c
userdocs <userdocs@digikool.co.uk>
2016-01-28 09:15:35 +0000

    monoapps

--- 0_templates/Bash_Scripts/cronscript.sh
+++ 0_templates/Bash_Scripts/cronscript.sh
@@ -3,5 +3,6 @@
 if [[ -z "$(screen -ls APPNAME | sed -rn 's/[^\s](.*).APPNAME(.*)/\1/p')" ]]
 then
     screen -dmS APPNAME ~/bin/mono --debug PATH
-    echo "APPNAME was restarted"; echo
+    echo "Restarted at: $(date +"%H:%M on the %d.%m.%y")" >> ~/.userdocs/cronjobs/logs/APPNAME.log 2>&1
+    exit
 fi
