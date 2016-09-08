c84bcbcca9166ba37b79d8ba749e1854c15e4ebe
userdocs <userdocs@digikool.co.uk>
2016-09-08 20:02:21 +0100

    updates

--- local_programs/Windows/nwgat/lftp-4.7.3.win64/runme - lftpsync.bat
+++ local_programs/Windows/nwgat/lftp-4.7.3.win64/runme - lftpsync.bat
@@ -1,1 +1,1 @@
-start bin\ConEmu64.exe -LoadCfgFile bin\ConEmu.xml -Min -TSA /dir %cd%\scripts -cmd bash lftpsync.sh -cur_console:n
+start bin\ConEmu64.exe -LoadCfgFile bin\ConEmu.xml -Min -TSA /dir %cd%\scripts -cmd ..\bin\bash.exe lftpsync.sh -cur_console:n
