b2d739417d767e159c8bfe1b8a403fa3ca427025
userdocs <userdocs@digikool.co.uk>
2017-05-23 21:25:47 +0100

    flood

--- 0_templates/Bash_Scripts/cronscript.sh
+++ 0_templates/Bash_Scripts/cronscript.sh
@@ -3,7 +3,7 @@
 if [[ -z "$(screen -ls APPNAME | sed -rn 's/[^\s](.*).APPNAME(.*)/\1/p')" ]]
 then
     # screen command
-	echo "$(screen -ls APPNAME | sed -rn 's/[^\s](.*).APPNAME(.*)/\1/p')" > ~/.userdocs/pids/APPNAME.pid
+	echo -n "$(screen -ls APPNAME | sed -rn 's/[^\s](.*).APPNAME(.*)/\1/p')" > ~/.userdocs/pids/APPNAME.pid
     echo "Restarted at: $(date +"%H:%M on the %d.%m.%y")" >> ~/.userdocs/cronjobs/logs/APPNAME.log 2>&1
     exit
 fi
