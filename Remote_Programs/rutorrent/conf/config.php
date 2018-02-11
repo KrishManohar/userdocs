e555532635549636b7b6c8a371d4b30ec679e6a8
userdocs <16525024+userdocs@users.noreply.github.com>
2018-02-11 15:02:18 +0000

    feral specific modifications

--- Remote_Programs/rutorrent/conf/config.php
+++ Remote_Programs/rutorrent/conf/config.php
@@ -1,13 +1,17 @@
 <?php
+
+    $_uname = posix_uname();
+    $_pw = posix_getpwuid(posix_geteuid());
+
 	// configuration parameters
 
 	// for snoopy client
-	@define('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0', true);
+	@define('HTTP_USER_AGENT', 'Mozilla/5.0 (Linux x86_64; '.$_uname['nodename'].'; compatible) rutorrent/tag-3.8', true);
 	@define('HTTP_TIME_OUT', 30, true);	// in seconds
 	@define('HTTP_USE_GZIP', true, true);
 	$httpIP = null;				// IP string. Or null for any.
 
-	@define('RPC_TIME_OUT', 5, true);	// in seconds
+	@define('RPC_TIME_OUT', 10, true);	// in seconds
 
 	@define('LOG_RPC_CALLS', false, true);
 	@define('LOG_RPC_FAULTS', true, true);
@@ -19,16 +23,16 @@
 	$schedule_rand = 10;			// rand for schedulers start, +0..X seconds
 
 	$do_diagnostic = true;
-	$log_file = '/tmp/errors.log';		// path to log file (comment or leave blank to disable logging)
+    $log_file = 'error.log';		// path to log file (comment or leave blank to disable logging)
 
 	$saveUploadedTorrents = true;		// Save uploaded torrents to profile/torrents directory or not
 	$overwriteUploadedTorrents = false;     // Overwrite existing uploaded torrents in profile/torrents directory or make unique name
 
-	$topDirectory = '/';			// Upper available directory. Absolute path with trail slash.
+    $topDirectory = $_pw['dir'];			// Upper available directory. Absolute path with trail slash.
 	$forbidUserSettings = false;
 
-	$scgi_port = 5000;
-	$scgi_host = "127.0.0.1";
+    $scgi_port = 0;
+    $scgi_host = 'unix://'.$_pw['dir'].'/private/rtorrent/.socket';
 
 	// For web->rtorrent link through unix domain socket 
 	// (scgi_local in rtorrent conf file), change variables 
@@ -37,7 +41,7 @@
 	// $scgi_port = 0;
 	// $scgi_host = "unix:///tmp/rpc.socket";
 
-	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
+    $XMLRPCMountPoint = '//rutorrent:@'.gethostname().'/'.$_pw['name'].'/RPC';
 
 	$pathToExternals = array(
 		"php" 	=> '',			// Something like /usr/bin/php. If empty, will be found in PATH.
@@ -53,7 +57,7 @@
 	);
 
 	$profilePath = '../share';		// Path to user profiles
-	$profileMask = 0777;			// Mask for files and directory creation in user profiles.
+	$profileMask = 0700;			// Mask for files and directory creation in user profiles.
 						// Both Webserver and rtorrent users must have read-write access to it.
 						// For example, if Webserver and rtorrent users are in the same group then the value may be 0770.
 
