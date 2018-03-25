2ddba9c0993614d9676c2af693c8e61ca68672d4
userdocs <16525024+userdocs@users.noreply.github.com>
2018-03-25 20:39:13 +0100

    proftpd cronscript

--- Remote_Programs/proftpd/cron/cronscript.sh
+++ Remote_Programs/proftpd/cron/cronscript.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #
-[[ $(cat ~/proftpd/sftp.pid) -eq $(ps x | pgrep proftpd) ]] || echo no
-[[ $(cat ~/proftpd/ftps.pid) -eq $(ps x | pgrep proftpd) ]] || echo no
+# This file is commented out so as not to start proftpd until we are ready and it is configured.
+#
+# A simple check to see if proftpd is running based on the pid file.
+#[[ $(cat ~/proftpd/sftp.pid) -eq $(ps x | pgrep proftpd) && -n $(cat ~/proftpd/sftp.pid 2> /dev/null) ]] || ~/proftpd/sbin/proftpd -c ~/proftpd/etc/sftp.conf
+#[[ $(cat ~/proftpd/ftps.pid) -eq $(ps x | pgrep proftpd) && -n $(cat ~/proftpd/sftp.pid 2> /dev/null) ]] || ~/proftpd/sbin/proftpd -c ~/proftpd/etc/ftps.conf
+#
