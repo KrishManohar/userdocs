4fbe0f2e68b6ecddb3d9f386049f08759760d259
userdocs <16525024+userdocs@users.noreply.github.com>
2018-04-02 10:03:02 +0100

    modules

--- modules/cronscripts/rtorrent-autodl.sh
+++ modules/cronscripts/rtorrent-autodl.sh
@@ -1,56 +1,48 @@
 #!/bin/bash
 #
 # Part 1: A cron based restarter for a custom instance of rtorrent and autodl.
-# Part 2: A cron based auto-patcher for an installation of autodl to a custom instance using the userdocs script.
-#
-#
-# The suffix is set here to be used throughout this cronscript.
-# suffix=""
 #
 # Please set the path to your www root here.
 wwwurl="$HOME/www/$(whoami).$(hostname -f)/public_html"
 #
 # The restart job for your custom rtorrent installation.
-#
-# We only want one instance of he named screen process. If there are more kill them off and restart. Otherwise do nothing.
-[[ "$(screen -ls rtorrent | grep -Ecw "rtorrent\s")" -ne '1' ]] && kill -9 $(echo $(screen -ls rtorrent | sed -rn 's/[^\s](.*).rtorrent[\t](.*)[\t](.*)/\1/p')) > /dev/null 2>&1 && screen -wipe > /dev/null 2>&1
-# Kill dead rtorrent screens as they might linger and cause later checks to give a false positive. Otherwise do nothing.
-[[ "$(screen -ls rtorrent | sed -rn 's/[^\s](.*).rtorrent[\t](.*)[\t](.*)/\3/p')" == '(Dead ???)' ]] && screen -wipe > /dev/null 2>&1
-# Check to see if all 3 required services are running. If not then kill the remaining, delete the lock file if present so the script can then restart the programs.
-if [[ "$(ps x | grep -Ecw "rtorrent$" | awk '{print $1}')" -ne '3' ]]; then
-    kill -9 $(echo $(ps x | grep -Ew "rtorrent$" | awk '{print $1}')) > /dev/null 2>&1
+if [[ "$(ps -xU $(whoami) | grep -Ev 'screen (.*) rtorrent' | grep -Ecw "rtorrent$" | awk '{print $1}')" -ne '3' && -d "$HOME/private/rtorrent" && ! -f "$HOME/.userdocs/tmp/rtorrent.lock" ]]; then
+    #
+    touch "$HOME/.userdocs/tmp/rtorrent.lock"
+    #
+    kill -9 $(echo $(ps -xU $(whoami) | grep -Ew "rtorrent$" | awk '{print $1}')) > /dev/null 2>&1 && screen -wipe > /dev/null 2>&1
+    #
     [[ -f "$HOME/private/rtorrent/work/rtorrent.lock" ]] && rm -f "$HOME/private/rtorrent/work/rtorrent.lock"
-fi
-#
-if [[ "$(ps x | grep -Ecw "rtorrent$" | awk '{print $1}')" -ne '3' && ! -f "$HOME/private/rtorrent/work/rtorrent.lock" && -d "$HOME/private/rtorrent" ]]; then
-    kill -9 $(echo $(ps x | grep -Ew "rtorrent$" | awk '{print $1}')) > /dev/null 2>&1
-    screen -wipe > /dev/null 2>&1
+    #
     screen -dmS "rtorrent" && screen -S "rtorrent" -p 0 -X stuff "rtorrent^M"
-    echo -n "$(echo $(ps x | grep -Ew "rtorrent$" | awk '{print $1}'))" > "$HOME/.userdocs/pids/rtorrent.pid"
+    #
+    echo -n "$(echo $(ps -xU $(whoami) | grep -Ew "rtorrent$" | grep -Ev 'screen (.*) rtorrent' | awk '{print $1}'))" > "$HOME/.userdocs/pids/rtorrent.pid"
     echo "Restarted at: $(date +"%H:%M on the %d.%m.%y")" >> "$HOME/.userdocs/cronjobs/logs/rtorrent.log" 2>&1
+    #
+    [[ -f "$HOME/.userdocs/tmp/rtorrent.lock" ]] && rm -f "$HOME/.userdocs/tmp/rtorrent.lock"
+    #
     exit
 fi
 #
 # The restart job for your custom autodl installation.
 #
-# We only want one instance of he named screen process. If there are more kill them off and restart. Otherwise do nothing.
-[[ "$(screen -ls autodl | grep -Ecw "autodl\s")" -ne '1' ]] && kill -9 $(echo $(screen -ls autodl | sed -rn 's/[^\s](.*).autodl[\t](.*)[\t](.*)/\1/p')) > /dev/null 2>&1 && screen -wipe > /dev/null 2>&1
-# Kill dead autodl screens as they might linger and cause later checks to give a false positive. Otherwise do nothing.
-[[ "$(screen -ls autodl | sed -rn 's/[^\s](.*).autodl[\t](.*)[\t](.*)/\3/p')" == '(Dead ???)' ]] && screen -wipe > /dev/null 2>&1
-# Check to see if all 2 required services are running. If not then kill the remaining so the script can then restart the programs.
-if [[ "$(ps x | grep -Ecw "(autodl$|irssi$)" | awk '{print $1}')" -ne '2' ]]; then
-    kill -9 $(echo $(ps x | grep -Ew "(autodl$|irssi$)" | awk '{print $1}')) > /dev/null 2>&1
-fi
-#
-if [[ "$(ps x | grep -Ecw "(autodl$|irssi$)" | awk '{print $1}')" -ne '2' && -d "$HOME/.autodl" && -d "$HOME/.irssi" ]]; then
-    kill -9 $(echo $(ps x | grep -Ew "(autodl$|irssi$)" | awk '{print $1}')) > /dev/null 2>&1
-    screen -wipe > /dev/null 2>&1
+if [[ "$(ps -xU $(whoami) | grep -Ev 'screen (.*) autodl' | grep -Ecw '(autodl$|irssi$)' | awk '{print $1}')" -ne '2' && -d "$HOME/.autodl" && -d "$HOME/.irssi" && ! -f "$HOME/.userdocs/tmp/autodl.lock" ]]; then
+    #
+    touch "$HOME/.userdocs/tmp/autodl.lock"
+    #
+    kill -9 $(echo $(ps -xU $(whoami) | grep -Ew "(autodl$|irssi$)" | awk '{print $1}')) > /dev/null 2>&1 && screen -wipe > /dev/null 2>&1
     screen -dmS "autodl" && screen -S "autodl" -p 0 -X stuff "irssi^M"
     screen -S "autodl" -p 0 -X stuff '/autodl update^M'
-    echo -n "$(echo $(ps x | grep -Ew "(autodl$|irssi$)" | awk '{print $1}'))" > "$HOME/.userdocs/pids/autodl.pid"
+    #
+    echo -n "$(echo $(ps -xU $(whoami) | grep -Ew "(autodl$|irssi$)" | awk '{print $1}'))" > "$HOME/.userdocs/pids/autodl.pid"
     echo "Restarted at: $(date +"%H:%M on the %d.%m.%y")" >> "$HOME/.userdocs/cronjobs/logs/autodl.log" 2>&1
+    #
+    [[ -f "$HOME/.userdocs/tmp/autodl.lock" ]] && rm -f "$HOME/.userdocs/tmp/autodl.lock"
+    #
     exit
 fi
+#
+# Part 2: A cron based auto-patcher for an installation of autodl to a custom instance using the userdocs script.
 #
 # Some checks to see what the current host settings are hard coded to. If the program updated it will be set to 127.0.0.1 otherwise it will be 10.0.0.1
 [[ -f "$HOME/.irssi/scripts/AutodlIrssi/GuiServer.pm" ]] && autodlfix="$(sed -rn "s#(.*)'(.*)';#\2#p" $HOME/.irssi/scripts/AutodlIrssi/GuiServer.pm)"
@@ -62,4 +54,6 @@ fi
 # Autodl Rutorrent host patch
 [[ "$rutorrentfix" = '127.0.0.1' ]] && sed -i 's|if (!socket_connect($socket, "127.0.0.1", $autodlPort))|if (!socket_connect($socket, "10.0.0.1", $autodlPort))|g' "$wwwurl/rutorrent/plugins/autodl-irssi/getConf.php" && updated="1"
 #
-[[ "$updated" -eq "1" ]] && screen -S "autodl-$suffix" -p 0 -X stuff '/run autorun/autodl-irssi.pl^M' && updated="0"
+[[ "$updated" -eq "1" ]] && screen -S "autodl" -p 0 -X stuff '/run autorun/autodl-irssi.pl^M' && updated="0"
+#
+exit
