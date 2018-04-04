2eebb25fe71a306af80c59eb2c6dc112accd6b0e
userdocs <16525024+userdocs@users.noreply.github.com>
2018-04-04 23:07:53 +0100

    plex

--- modules/plex/install.sh
+++ modules/plex/install.sh
@@ -1,4 +1,65 @@
 echo "Welcome to the Feral install plex script."
 echo
+while [[ -z "$username" ]]
+do
+    read -ep "Enter your plex account username: " username
+    echo
+done
+#
+while [[ -z "$password" ]]
+do
+    read -ep "Enter your plex account password: " password
+    echo
+done
+#
+echo 'You can check the latest version here: https://www.plex.tv/downloads/'
+#
+read -ep 'What plex version would you like to install (non plex pass): ' -i '1.12.1.4885-1046ba85f' plexversion
+#
+echo "Your username is: $username"
+echo "Your password is: $password"
+echo
+read -ep "Are these details correct, [y]es [n]o to reload this script and start again or [q]uit." confirm
+echo
+#
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    #
+    pkill -9 -fu "$(whoami)" 'plexmediaserver' > /dev/null 2>&1
+    pkill -9 -fu "$(whoami)" 'EAE Service' > /dev/null 2>&1
+    #
+    mkdir -p ~/.config/feral/ns/forwarding/tcp
+    grep -l 32400 ~/.config/feral/ns/forwarding/tcp/* | xargs rm -f > /dev/null 2>&1
+    #
+    [[ -d ~/Library ]] &&  rm -rf ~/Library
+    [[ -d ~/private/plex ]] && rm -rf ~/private/plex
+    #
+    mkdir -p ~/private/plex
+    #
+    echo "$plexversion" > ~/private/plex/.version
+    echo ''"$username"':'"$password"'' > ~/private/plex.login
+    #
+    while [ ! -f ~/private/plex.url ]; do printf '\rInstalling plex, please wait (May take up to 10min)...'; sleep 2; done
+    echo
+    echo "Please visit your plex installation on this url: $(cat ~/private/plex.url)"
+    echo
+    exit
+fi
+#
+if [[ "$confirm" =~ ^[Nn]$ ]]; then
+    bash ~/bin/install.plex
+    exit
+fi
+#
+if [[ "$confirm" =~ ^[Qq]$ ]]; then
+    echo "You chose to quit the script"
+    echo
+    exit
+fi
+#
+if [[ "$confirm" =~ ^[^YyQqNn].*?$ ]]; then
+    echo "A wild kitten walked over the keyboard, i will quit this script to be safe."
+    echo
+    exit
+fi
 #
 exit
