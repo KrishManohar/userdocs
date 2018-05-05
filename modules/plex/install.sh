50b79de2c3472c30e9d06e20aa665f6ea6d7354d
userdocs <16525024+userdocs@users.noreply.github.com>
2018-05-05 19:52:34 +0100

    plex

--- modules/plex/install.sh
+++ modules/plex/install.sh
@@ -4,6 +4,40 @@ if [[ -d ~/private/plex ]]; then
     read -ep "Do you want to update to $plexversion: " plexupdate
     echo
 fi
+if [[ "$plexupdate" =~ ^[Yy]$ ]]; then
+    #
+    wget -qO ~/plex.deb "$plexamd64url"
+    #
+    dpkg-deb -x ~/plex.deb ~/private/plex
+    #
+    [[ -f ~/plex.deb ]] && rm -f ~/plex.deb
+    #
+    pkill -9 -fu "$(whoami)" 'plexmediaserver' > /dev/null 2>&1
+    pkill -9 -fu "$(whoami)" 'EAE Service' > /dev/null 2>&1
+    #
+    #
+    if [[ "$(date +%-M)" -le '4' ]] && [[ "$(date +%-M)" -ge '0' ]]; then time="$(( 5 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '9' ]] && [[ "$(date +%-M)" -ge '5' ]]; then time="$(( 10 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '14' ]] && [[ "$(date +%-M)" -ge '10' ]]; then time="$(( 15 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '19' ]] && [[ "$(date +%-M)" -ge '15' ]]; then time="$(( 20 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '24' ]] && [[ "$(date +%-M)" -ge '20' ]]; then time="$(( 25 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '29' ]] && [[ "$(date +%-M)" -ge '25' ]]; then time="$(( 30 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '34' ]] && [[ "$(date +%-M)" -ge '30' ]]; then time="$(( 35 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '39' ]] && [[ "$(date +%-M)" -ge '35' ]]; then time="$(( 40 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '44' ]] && [[ "$(date +%-M)" -ge '40' ]]; then time="$(( 45 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '49' ]] && [[ "$(date +%-M)" -ge '45' ]]; then time="$(( 50 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '54' ]] && [[ "$(date +%-M)" -ge '50' ]]; then time="$(( 55 * 60 ))"; fi
+    if [[ "$(date +%-M)" -le '59' ]] && [[ "$(date +%-M)" -ge '55' ]]; then time="$(( 60 * 60 ))"; fi
+    #
+    while [[ "$(ps -xU $(whoami) | grep -Ev 'screen (.*) plex' | grep -Ecw "/bin/sh usr/lib/plexmediaserver/start.sh$")" -eq "0" ]]
+    do
+        countdown="$(( $time-$(($(date +%-M) * 60 + $(date +%-S))) ))"
+        printf '\rsomeapp will restart in approximately: %dm:%ds ' $(($countdown%3600/60)) $(($countdown%60))
+    done
+    echo -e '\n'
+    #
+    exit
+fi
 #
 while [[ -z "$username" ]]
 do
@@ -38,7 +72,7 @@ if [[ "$confirm" =~ ^[Yy]$ ]]; then
     mkdir -p ~/.config/feral/ns/forwarding/tcp
     grep -l 32400 ~/.config/feral/ns/forwarding/tcp/* | xargs rm -f > /dev/null 2>&1
     #
-    [[ -d ~/Library ]] &&  rm -rf ~/Library
+    [[ -d ~/Library ]] && rm -rf ~/Library
     [[ -d ~/private/plex ]] && rm -rf ~/private/plex
     #
     mkdir -p ~/private/plex
