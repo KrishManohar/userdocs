fcdbe4f1bdf17735a0aca90bb1cb13bbb043dc25
userdocs <16525024+userdocs@users.noreply.github.com>
2018-10-31 08:11:07 +0000

    housekeeping

--- modules/cronscripts/monoapps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-if [[ -z "$(screen -ls APPNAME | sed -rn 's/[^\s](.*).APPNAME(.*)/\1/p')" && ! -f "$HOME/.userdocs/tmp/APPNAME.lock" ]]
-then
-    # screen command
-	echo -n "$(screen -ls APPNAME | sed -rn 's/[^\s](.*).APPNAME(.*)/\1/p')" > ~/.userdocs/pids/APPNAME.pid
-    echo "Restarted at: $(date +"%H:%M on the %d.%m.%y")" >> ~/.userdocs/cronjobs/logs/APPNAME.log 2>&1
-    exit
-fi
